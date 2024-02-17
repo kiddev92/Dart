74c81fc777f7938d9ed94a9781e616e33421fdaf
kid <kiddev92@gmail.com>
2024-02-17 23:41:09 +0700

    main

--- bin/1. Variable.dart
+++ bin/1. Variable.dart
@@ -4,11 +4,15 @@ void main() {
   num dateYear = 2000;
   double weight = 50.3;
   bool isSingle = true;
+  final pet = "cat";
+  const country = "indonesia";
 
   print(name);
   print(age);
   print(dateYear);
   print(weight);
   print(isSingle);
+  print(pet);
+  print(country);
 
 }
