15fd8c230a0bb8a69b2b47437b0fca8f93bf4c28
kid <kiddev92@gmail.com>
2024-02-17 23:57:22 +0700

    main

--- bin/1. Variable.dart
+++ bin/1. Variable.dart
@@ -17,6 +17,11 @@ void main() {
   // Null Safety
   String? nickName; //Type Safety
   var castIntToString = age.toString(); //Type Cast
+  late String lateVariable;
+  lateVariable = "Late";
+
+  // Private Variable
+  String _password = "12345678";
 
   // Print Output
   print(hobby);
@@ -30,5 +35,7 @@ void main() {
   print(country); //const
   print(nickName);   //Type Safety
   print(castIntToString.runtimeType); //Type Cast
+  print(_password); // Private Variable
+  print(lateVariable); // Late Variable
 
 }
